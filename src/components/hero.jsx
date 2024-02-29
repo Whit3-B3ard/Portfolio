0ab65f39f8cbadef5e9fe10474cde2f968dd48c4
Ibrahim Mansour <Ibrahim0Mansour@gmail.com>
2024-02-29 13:13:30 +0100

    replacing profile pic

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -6,8 +6,11 @@ export default function Hero() {
     <section className="pb-10 md:pb-16">
       <div className="container max-w-screen-xl mx-auto px-4">
         <div className="text-center">
-          <div className="flex justify-center mb-16">
-            <img src="../../image/cropped-face.png" alt="Image"></img>
+          <div className="flex justify-center mb-10">
+            <img
+              src="../../image/bewerbungs_foto_cropped.png"
+              alt="Image"
+            ></img>
           </div>
 
           <h6 className="font-medium text-gray-600 text-lg md:text-2xl uppercase mb-8">
